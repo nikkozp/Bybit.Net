2134cc1584425cfd952a460e4b91afcdabcaeec2
Nikita <nikkozp@hotmail.com>
2024-02-06 12:38:51 +0100

    commit all repos

---
+++ ByBit.Net/Enums/SelfTradePreventionMode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bybit.Net.Enums
+{
+    public enum SelfTradePreventionMode
+    {
+        /// <summary>
+        /// No STP
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// expire taker order when STP triggers
+        /// </summary>
+        Expire_taker,
+
+        /// <summary>
+        /// expire taker order when STP triggers
+        /// </summary>
+        Expire_maker,
+
+        /// <summary>
+        /// expire both orders when STP triggers
+        /// </summary>
+        Expire_both
+    }
+}
